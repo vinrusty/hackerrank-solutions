bdd4042680c7fd29d84885bb9c612d4a09cec556
Vineeth <72906276+vinrusty@users.noreply.github.com>
2020-12-21 12:04:07 +0530

    Update SubArrayDivision.cpp

--- SubArrayDivision.cpp
+++ SubArrayDivision.cpp
@@ -1,36 +1,32 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
 using namespace std;
-
-
-int main() {
-    int n;
-    cin >> n;
-    int maximum;
-    int minimum;
-    int minCounter = 0;
-    int maxCounter = 0;
-    cin >> maximum;
-    minimum = maximum;
-
-    for (int i = 1; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-        if (tmp > maximum) {
-            maximum = tmp;
-            maxCounter++;
-        }
-
-        if (tmp < minimum) {
-            minimum = tmp;
-            minCounter++;
+int main(){
+    int n,d,m,j,k;
+    int s,count=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cin>>d>>m;
+    if(n>1){
+        for(j=0;j<=n-m;j++){
+            for(k=j;k<j+m;k++){
+                s = s + a[k];
+            }
+            if(s%d==0){
+                count++;
+            }
+            s = 0;
         }
     }
-
-    cout << maxCounter << " " << minCounter;
-
+    else{
+        if(a[0]==d){
+            count++;
+        }
+    }
+    cout<<count;
     return 0;
 }
